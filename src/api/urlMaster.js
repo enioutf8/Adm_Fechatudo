07d9ebe6624d6f9e394191622717c58d714ec276
enio.as14@outlook.com <enio.as14@outlook.com>
2026-01-28 15:07:42 -0300

    0.3.2

--- src/api/urlMaster.js
+++ src/api/urlMaster.js
@@ -2,7 +2,7 @@ export default class Urlmaster {
   getUrlMaster() {
     const urlApi = "https://fechatudo.com.br/api/";
     const urlSite = "https://fechatudo.com.br/";
-    const urlTemp = "http://localhost:3000/"
-    return { urlApi, urlSite, urlTemp };
+     
+    return { urlApi, urlSite  };
   }
 }
