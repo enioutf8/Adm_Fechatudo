79caa086d52303bb0c9253b98e1e00df98972ad3
enio.as14@outlook.com <enio.as14@outlook.com>
2026-01-28 15:12:27 -0300

    0.3.3

--- src/api/urlMaster.js
+++ src/api/urlMaster.js
@@ -2,7 +2,7 @@ export default class Urlmaster {
   getUrlMaster() {
     const urlApi = "https://fechatudo.com.br/api/";
     const urlSite = "https://fechatudo.com.br/";
-     
-    return { urlApi, urlSite  };
+
+    return { urlApi, urlSite };
   }
 }
